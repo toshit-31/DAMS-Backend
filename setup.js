536c79743e99a057ae65edd1afa93a73b894dd3a
Toshit Mall <65126601+toshit-31@users.noreply.github.com>
2023-04-01 06:41:04 -0700

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,5 +1,5 @@
 const dgraph = require("./controllers/dgraph-graphql.js")
-const schema = require("schema.js")
+const schema = require("./schema.js")
 
 dgraph.updateSchema(schema).then((res) =>{
   console.log(res)
